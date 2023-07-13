82dbcbce74ac973490cfd6317bd5cfad51675336
Denis Aksenov <BLIHOK@mail.ru>
2023-07-13 18:33:15 +0300

    New error solved

--- src/test/kotlin/DiscountCalcTest.kt
+++ src/test/kotlin/DiscountCalcTest.kt
@@ -14,7 +14,7 @@ class DiscountCalcTest {
         val transfer = 0
 
         val result = discountCalc.calcDiscountByCardType(amount, transfer)
-        assertEquals(100, result)
+        assertEquals(0, result)
     }
 
     @Test
